e11c151ce7908d5cfc6d391e17a19178d9cab129
James Stamps Jr <jstampshere@gmail.com>
2020-04-06 00:50:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,14 @@
 
 def using_concat(array1, array2)
   array3 = array1.concat(array2)
-  
 end
+
+def using_insert(array, element)
+  array.insert(element[4])
+end
+
+
+
 
 def using_uniq(array)
    array.uniq
